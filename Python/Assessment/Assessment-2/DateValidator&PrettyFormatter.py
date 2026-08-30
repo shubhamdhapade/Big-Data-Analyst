a50fc9978743c10685ff438023daf05bf8706cc9
Shubham Dhapade <shubhamdhapade@gmail.com>
2026-08-30 21:58:22 +0530

    Assessment-2: Date Validator&PrettyFormatter

---
+++ Python/Assessment/Assessment-2/DateValidator&PrettyFormatter.py
@@ -0,0 +1,86 @@
+'''
+    Date Validator & Pretty Formatter
+    Write a program that prompts the user to enter a date string in the format "DD/MM/YYYY".
+
+    Your program must:
+    Verify if the date is valid. To be valid:
+    The month must be between 1 and 12 inclusive.
+    The day must be valid for that specific month (e.g., April, June, September, November have 30 days; others have 31 days).
+    For February, the day must be at most 29 in a leap year (divisible by 4, except for centuries not divisible by 400) and at most 28 in standard years.
+    If the date is valid, use a tuple of month names ("January", "February", ...) to format and print the date in a long-form readable layout: "MonthName DD, YYYY".
+    If the date is invalid, print "Invalid Date".
+    Sample Input: "26/08/2026"
+    Sample Output: "August 26, 2026"
+    Sample Input: "29/02/2026" (2026 is not a leap year)
+    Sample Output: "Invalid Date"
+    Sample Input: "31/04/2026" (April only has 30 days)
+    Sample Output: "Invalid Date"
+'''
+
+import os
+
+monthNames = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+}
+
+def isValidMonth(month):
+    if 1 <=  month <= 12:
+        return True
+    else:
+        return False
+
+def isLeapYear(year):
+    if year % 400 == 0 or year % 4 == 0 and year % 100 != 0:
+        # Leap Year
+        return True
+    else: 
+        return False
+
+def isVaildDate(day, month, year):
+    if not isValidMonth(month):
+        return False
+    if day < 1 or day > 31:
+        # Invalid Date
+        return False
+    if month == 2 :
+        max_days = 29 if isLeapYear(year) else 28
+        if day < max_days:
+            return False
+    if month in (4, 6, 9, 11):
+        if day  > 30:
+            return False
+    return True
+
+def checkDateFormat(date):
+    parts = date.split("/")
+    if len(parts) != 3:
+        return False
+    return True
+
+def main():
+    date  = input("Enter the date  in format (DD/MM/YYYY) : ")
+    if not checkDateFormat(date):
+        print("Invalid Date")
+        return
+    day, month, year = map(int, date.split("/"))
+    if isVaildDate(day, month, year):
+        print(f"{monthNames[month - 1]} {day}, {year}")
+    else:
+        print("Invalid Date")
+if __name__ == "__main__":
+    os.system("cls")
+    print("*" * 80)
+    print(f"{' Date Validator & Pretty Formatter ':^80}")
+    print("-" * 80)
+    main()
